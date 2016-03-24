b0623751b10378919bca9617ad33ae0faea17582
nong053 <kosit.arom@gmail.com>
2016-03-25 05:51:35 +0700

    edit layout design_trend

--- resources/views/ais/design_trend.blade.php
+++ resources/views/ais/design_trend.blade.php
@@ -88,7 +88,7 @@
                                     <input type="text" name="search" class="form-control"
                                            placeholder="ค้นหา" value="{{session()->get('design_trend_search')}}">
                                 </div>
-                                <div class="col-md-3" style="margin-top: 8px;width: 180px">
+                                <div class="col-md-3" style="margin-top: 8px;width: 190px">
 
                                     Sort By:
                                     <!--  btn -->
